3247dc33ae98587f576c4b49d943dd5939d1c144
Holladiewal <holladiewal@whydoyouhate.me>
2022-05-28 14:46:51 +0200

    Adjust test.sh to use pretrain config instead of finetune config

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-python3 tools/test_net.py --config-file configs/finetune.yaml #
+#python3 tools/test_net.py --config-file configs/finetune.yaml #
+python3 tools/test_net.py --config-file configs/pretrain.yaml #
