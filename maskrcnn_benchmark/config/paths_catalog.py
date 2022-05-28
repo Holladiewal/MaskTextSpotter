e0e14ded73012b1f11c5330ebe9d20861dd8d2a9
Holladiewal <holladiewal@whydoyouhate.me>
2022-05-28 14:35:25 +0200

    Reformatted files

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -3,15 +3,17 @@
 
 import os
 
+
 def add_gt_path(data_dir, attrs):
-    if len(attrs)>1:
-        return os.path.join(data_dir,attrs[1])
+    if len(attrs) > 1:
+        return os.path.join(data_dir, attrs[1])
     else:
         return None
 
+
 class DatasetCatalog(object):
     DATA_DIR = "datasets"
-
+    
     DATASETS = {
         "coco_2014_train": (
             "coco/train2014",
@@ -26,34 +28,34 @@ class DatasetCatalog(object):
             "coco/val2014",
             "coco/annotations/instances_valminusminival2014.json",
         ),
-        "icdar_2013_train":(
+        "icdar_2013_train": (
             "icdar2013/train_images",
             "icdar2013/train_gts",
-
+        
         ),
-        "icdar_2013_test":(
+        "icdar_2013_test": (
             "icdar2013/test_images",
             "icdar2013/test_gts",
         ),
         "icdar_2015_train": (
             "icdar2015/train_images",
             "icdar2015/train_gts",
-
+        
         ),
         "icdar_2015_test": (
             "icdar2015/test_images",
             # "icdar2015/test_gts",
-
+        
         ),
-        "synthtext_train":(
+        "synthtext_train": (
             "synthtext/train_images",
             "synthtext/train_gts",
-
+        
         ),
         "synthtext_test": (
             "synthtext/test_images",
             "synthtext/test_gts",
-
+        
         ),
         "total_text_train": (
             "total_text/train_images",
@@ -63,13 +65,13 @@ class DatasetCatalog(object):
             "total_text/test_images",
             # "total_text/test_gts",
         ),
-        "scut-eng-char_train":(
+        "scut-eng-char_train": (
             "scut-eng-char/train_images",
             "scut-eng-char/train_gts",
         ),
-
+        
     }
-
+    
     @staticmethod
     def get(name):
         if "coco" in name:
@@ -85,11 +87,11 @@ class DatasetCatalog(object):
             )
         elif "icdar_2013" in name:
             data_dir = DatasetCatalog.DATA_DIR
-            attrs=DatasetCatalog.DATASETS[name]
-            args=dict(
+            attrs = DatasetCatalog.DATASETS[name]
+            args = dict(
                 use_charann=True,
-                imgs_dir=os.path.join(data_dir,attrs[0]),
-                gts_dir=add_gt_path(data_dir,attrs),
+                imgs_dir=os.path.join(data_dir, attrs[0]),
+                gts_dir=add_gt_path(data_dir, attrs),
             )
             return dict(
                 args=args,
@@ -97,14 +99,14 @@ class DatasetCatalog(object):
             )
         elif "icdar_2015" in name:
             data_dir = DatasetCatalog.DATA_DIR
-            attrs=DatasetCatalog.DATASETS[name]
-            if len(attrs)>1:
-                gts_dir = os.path.join(data_dir,attrs[1])
+            attrs = DatasetCatalog.DATASETS[name]
+            if len(attrs) > 1:
+                gts_dir = os.path.join(data_dir, attrs[1])
             else:
                 gts_dir = None
-            args=dict(
+            args = dict(
                 use_charann=False,
-                imgs_dir=os.path.join(data_dir,attrs[0]),
+                imgs_dir=os.path.join(data_dir, attrs[0]),
                 gts_dir=gts_dir,
             )
             return dict(
@@ -113,12 +115,12 @@ class DatasetCatalog(object):
             )
         elif "synthtext" in name:
             data_dir = DatasetCatalog.DATA_DIR
-            attrs=DatasetCatalog.DATASETS[name]
-            args=dict(
+            attrs = DatasetCatalog.DATASETS[name]
+            args = dict(
                 use_charann=True,
                 list_file_path=os.path.join(data_dir, 'synthtext/train_list.txt'),
-                imgs_dir=os.path.join(data_dir,attrs[0]),
-                gts_dir=add_gt_path(data_dir,attrs),
+                imgs_dir=os.path.join(data_dir, attrs[0]),
+                gts_dir=add_gt_path(data_dir, attrs),
             )
             return dict(
                 args=args,
@@ -127,8 +129,8 @@ class DatasetCatalog(object):
         elif "total_text" in name:
             data_dir = DatasetCatalog.DATA_DIR
             attrs = DatasetCatalog.DATASETS[name]
-            if len(attrs)>1:
-                gts_dir = os.path.join(data_dir,attrs[1])
+            if len(attrs) > 1:
+                gts_dir = os.path.join(data_dir, attrs[1])
             else:
                 gts_dir = None
             args = dict(
@@ -162,7 +164,7 @@ class ModelCatalog(object):
     #     "MSRA/R-101": "ImageNetPretrained/MSRA/R-101.pkl",
     #     "FAIR/20171220/X-101-32x8d": "ImageNetPretrained/20171220/X-101-32x8d.pkl",
     # }
-
+    
     # C2_DETECTRON_SUFFIX = "output/train/coco_2014_train%3Acoco_2014_valminusminival/generalized_rcnn/model_final.pkl"
     # C2_DETECTRON_MODELS = {
     #     "35857197/e2e_faster_rcnn_R-50-C4_1x": "01_33_49.iAX0mXvW",
@@ -184,7 +186,7 @@ class ModelCatalog(object):
         "MSRA/R-101-GN": "ImageNetPretrained/47592356/R-101-GN.pkl",
         "FAIR/20171220/X-101-32x8d": "ImageNetPretrained/20171220/X-101-32x8d.pkl",
     }
-
+    
     C2_DETECTRON_SUFFIX = "output/train/{}coco_2014_train%3A{}coco_2014_valminusminival/generalized_rcnn/model_final.pkl"
     C2_DETECTRON_MODELS = {
         "35857197/e2e_faster_rcnn_R-50-C4_1x": "01_33_49.iAX0mXvW",
@@ -199,7 +201,7 @@ class ModelCatalog(object):
         # keypoints
         "37697547/e2e_keypoint_rcnn_R-50-FPN_1x": "08_42_54.kdzV35ao"
     }
-
+    
     @staticmethod
     def get(name):
         if name.startswith("Caffe2Detectron/COCO"):
@@ -207,7 +209,7 @@ class ModelCatalog(object):
         if name.startswith("ImageNetPretrained"):
             return ModelCatalog.get_c2_imagenet_pretrained(name)
         raise RuntimeError("model not present in the catalog {}".format(name))
-
+    
     @staticmethod
     def get_c2_imagenet_pretrained(name):
         prefix = ModelCatalog.S3_C2_DETECTRON_URL
@@ -215,7 +217,7 @@ class ModelCatalog(object):
         name = ModelCatalog.C2_IMAGENET_MODELS[name]
         url = "/".join([prefix, name])
         return url
-
+    
     @staticmethod
     def get_c2_detectron_12_2017_baselines(name):
         # Detectron C2 models are stored following the structure
